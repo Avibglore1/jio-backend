bc44c59e7c5143f90c3b522e4941636a0c0f7b42
Avinash <avinashkumarofficial95@gmail.com>
2025-04-05 00:01:12 +0530

    changed cors fromtend url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: "https://jio-frontend-rney-biate7xn1.vercel.app/", // Frontend URL
     methods: "GET,POST,PUT,DELETE",
     credentials: true, // Allow cookies, authorization headers, etc.
   })
