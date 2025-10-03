ee79fa31f0322625951c62f0afbdce7208b5a46d
Avinash <avinashkumarofficial95@gmail.com>
2025-10-03 13:05:02 +0530

    Fix: Correct import path for PaymentController.js to match case sensitivity

--- Routers/PaymentRouter.js
+++ Routers/PaymentRouter.js
@@ -3,7 +3,7 @@ export const PaymentRouter = express.Router();
 import {
     getPaymentController,
     updatePremiumAccessController,
-} from "../controllers/PaymentController.js";
+} from "../controllers/paymentcontroller.js";
 
 PaymentRouter.post("/order", getPaymentController);
 PaymentRouter.patch("/update-premium-access", updatePremiumAccessController);
